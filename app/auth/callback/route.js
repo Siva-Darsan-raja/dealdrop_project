824ba65ba15b79a44704ea1bf88af466dee5a553
Siva Darsan <sivadarsan48@gmail.com>
2026-01-03 21:28:30 +0530

    Update route.js

--- app/auth/callback/route.js
+++ app/auth/callback/route.js
@@ -4,17 +4,11 @@ import { NextResponse } from "next/server";
 export async function GET(request) {
   const { searchParams } = new URL(request.url);
   const code = searchParams.get("code");
-  const next = searchParams.get("next") ?? "/";
 
   if (code) {
     const supabase = await createClient();
-    const { error } = await supabase.auth.exchangeCodeForSession(code);
-
-    if (!error) {
-      return NextResponse.redirect(new URL(next, request.url));
-    }
-  }
+    await supabase.auth.exchangeCodeForSession(code);
 
   // Return the user to an error page with instructions
-  return NextResponse.redirect(new URL("/error", request.url));
+  return NextResponse.redirect(new URL("/", request.url));
 }
